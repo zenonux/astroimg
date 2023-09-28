db85def8a228e16033e84567b5d129cef4ffc0b5
ZenoQi <zenonux@outlook.com>
2023-09-28 17:25:59 +0800

    chore: uploader

--- play/src/main.ts
+++ play/src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import "element-plus/theme-chalk/index.css";
 import App from "./App.vue";
 const app = createApp(App);
+app.use(ElementPlus);
 app.mount("#app");
