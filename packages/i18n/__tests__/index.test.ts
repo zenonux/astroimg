a9e78982b622b2327c78853ea98b5a05bc7a8f96
qiyunfei <zeno.qi@zwoptical.com>
2025-04-28 17:04:53 +0800

    chore: test

--- packages/i18n/__tests__/index.test.ts
+++ packages/i18n/__tests__/index.test.ts
@@ -2,6 +2,25 @@ import path from "node:path";
 import { mergeLocales } from "../src";
 import { test } from "bun:test";
 import key from "./key.json";
+import { promisify } from "node:util";
+import { exec } from "node:child_process";
+const execPromise = promisify(exec);
+
+
+
+const cli = (argv: string) => {
+  return execPromise(
+    `bun ${path.resolve(__dirname, "../src/dist")} ${argv}`
+  )
+    .then((res) => [null, res.stdout])
+    .catch((e) => [e, null]);
+};
+
+
+test("cli check", async() => {
+    await cli("check -c ./i18n.yaml");
+});
+
 
 test("mergeLocales", async () => {
   let locales = path.resolve("locales");
