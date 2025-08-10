effc809ba75e20ee6d1f092e69dcfffcaf7b4fac
zenonux <zenonux@outlook.com>
2025-08-10 17:43:12 +0800

    chore(i18n): test

---
+++ packages/i18n/tests/check.test.ts
@@ -0,0 +1,18 @@
+import { resolve } from "node:path";
+import { test, expect } from "bun:test";
+import { promisify } from "node:util";
+import { exec } from "node:child_process";
+const execPromise = promisify(exec);
+
+const cli = (argv: string) => {
+  return execPromise(`bun ${resolve(__dirname, "../src/cli")} ${argv}`)
+    .then((res) => [null, res.stdout])
+    .catch((e) => [e, null]);
+};
+
+test("cli check", async () => {
+  let [err] = await cli("check -c ./i18n.yaml");
+  expect(err).toBe(null);
+});
+
+
