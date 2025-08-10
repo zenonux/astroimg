8e265ae1e49bdd376eed624c23488c2acb81d090
zenonux <zenonux@outlook.com>
2025-08-10 19:42:49 +0800

    fix(i18n): add index.d.ts

---
+++ packages/i18n/dist/index.d.ts
@@ -0,0 +1,14 @@
+import type { Plugin } from 'vite';
+
+declare function ViteI18nGeneratePlugin(
+  /**
+   * 监听的请求路径，比如 '/i18n-generate'，必须以 `/` 开头
+   */
+  route: string,
+
+  /**
+   * 要执行的命令字符串，比如 'npm run i18n:generate'
+   */
+  command: string
+): Plugin;
+
